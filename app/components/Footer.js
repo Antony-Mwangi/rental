3557a847d55c10fd3664f720174b1288a7f26f7f
Antony Mwangi <antonymwangiw85@gmail.com>
2025-12-22 23:16:07 +0300

    Initial commit

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "10px", backgroundColor: "#333", color: "#fff", textAlign: "center" }}>
+      Rental System &copy; 2025
+    </footer>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h1>Welcome to the Rental System</h1>
+        <p>Find your perfect property to rent easily.</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
